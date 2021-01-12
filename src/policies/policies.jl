06551ff3105b8aca67f0c78ae45e7a58dc3f05b1
Douglas Corbin <dfcorbin98@gmail.com>
2021-01-12 21:56:14 +0000

    Restrucure files

--- src/policies/policies.jl
+++ src/policies/policies.jl
@@ -1,5 +1,6 @@
 abstract type BanditPolicy end
 
 include("RandPol.jl")
-include("ThompsonSampling.jl")
+include("ThompsonSampling/ThompsonSampling.jl")
+include("ThompsonSampling/recursive_partition_TS.jl")
 include("KNN_UCB.jl")
