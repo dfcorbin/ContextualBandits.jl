de8fd0e34178d485c45de4c188a8e5ef633cfeed
Douglas Corbin <dfcorbin98@gmail.com>
2021-01-15 00:08:47 +0000

    Delete gitpush.sh

--- gitpush.sh
+++
@@ -1,9 +0,0 @@
-# Format code before deployment
-
-echo "Formatting julia code..."
-julia -e 'using JuliaFormatter; format(".")'
-
-# Restage changes
-git add $(git diff --name-only --cached)
-git commit -m "$1"
-git push
