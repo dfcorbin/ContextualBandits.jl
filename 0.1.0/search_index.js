2a520b83628c40b95ae863c1d8fcf5cabda9bdc6
Documenter.jl <documenter@juliadocs.github.io>
2021-01-15 00:21:58 +0000

    build based on de8fd0e

---
+++ 0.1.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ContextualBandits","category":"page"},{"location":"#ContextualBandits","page":"Home","title":"ContextualBandits","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ContextualBandits]","category":"page"}]
+}
